1aab41293b9b2534b595c3c20f14044d14f81ec1
wswiecho <wswiecho@stanford.edu>
2017-02-25 21:39:22 -0800

    Update BB1.m

--- HW3/BB1.m
+++ HW3/BB1.m
@@ -101,8 +101,13 @@ while iter < MAX_ITER
 end
 
 figure();
+subplot(2,1,1)
 plot(1:iter, gvals, 'LineWidth',2); grid on;
-title('Objective Function BB'); xlabel('Iteration'); ylabel('F(x)');
+title('Objective Function BB'); xlabel('Iteration'); ylabel('f(x)');
+
+subplot(2, 1, 2)
+plot(1:iter, fvals, 'LineWidth',2); grid on;
+title('Objective Function BB with Regularization'); xlabel('Iteration'); ylabel('F(x)');
 
 
 
