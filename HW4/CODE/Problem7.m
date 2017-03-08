0d2213662ad7893876d587a75c9d1e2c24908c67
Jacob Perricone <jacobperricone@gmail.com>
2017-03-08 11:57:39 -0800

    Made Code Directory

---
+++ HW4/CODE/Problem7.m
@@ -0,0 +1,32 @@
+clear all
+close all
+
+%% Problem 7
+% Part (a) and (b)
+
+A = [1, 1, 1; 1, 1, 2; 1, 2, 2];
+beta = [.1, 1, 10];
+
+for i=1:3
+    % Without the objective function.
+    ADMM(0, beta(i), 100, A)
+    
+    % With the objective function.
+    ADMM(1, beta(i), 100, A)
+end
+
+
+
+
+%% Random Permutation
+% Part (c)
+
+for i=1:3
+    
+    % Without the objective function and with permuation.
+    PermADMM(0, beta(i), 100, A) 
+    
+    % With objective function and permuation.
+    PermADMM(1, beta(i), 100, A) 
+  
+end
