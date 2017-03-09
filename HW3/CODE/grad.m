366a60727f5ef14ee66ce3f1403c0e5d56f1253f
wswiecho <wswiecho@stanford.edu>
2017-03-09 13:37:21 -0800

    Update grad.m

--- HW3/CODE/grad.m
+++ HW3/CODE/grad.m
@@ -1,3 +1,5 @@
+% Retuns: scalar value of the gradient function of x.
+
 function [ val ] = grad( x, a, d )
-val = -4*sum((repmat(dot(a-x, a-x) - d.^2, 2, 1) .* (a-x))')';
+    val = -4*sum((repmat(dot(a-x, a-x) - d.^2, 2, 1) .* (a-x))')';
 end
