cc08a03443e01b3a6d9739f8a9ec23cdebab6de8
wswiecho <wswiecho@stanford.edu>
2017-03-09 13:36:41 -0800

    Update gradx2.m

--- HW3/CODE/gradx2.m
+++ HW3/CODE/gradx2.m
@@ -1,3 +1,6 @@
+%
+% Returns: 2x1 vector corresponding to the gradent of x2.
+
 function [val] = gradx2(a, d, x1, x2)
 val = -4*(norm(a(:, 2) - x2)^2 - d(3)^2) * (a(:, 2) - x2) ....
     -4*(norm(a(:, 3) - x2)^2 - d(4)^2) * (a(:, 3) - x2) ...
