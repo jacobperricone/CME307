e3a1324fb87f3456911b08e8609e6a9cc43ae3d3
wswiecho <wswiecho@stanford.edu>
2017-03-09 10:16:11 -0800

    Update setup_problem_client.m

--- project/Code/setup_problem_client.m
+++ project/Code/setup_problem_client.m
@@ -2,13 +2,20 @@ clear all
 close all
 
 
-%% declare the anchors
-anchors = [1, -1, 0; 0, 0, 2];
+%% generates anchors
+random_x = [3*rand(1,6)-1.5]';
+random_y = [3*rand(1,6)-1.5]';
+% creates the convexhull around (x,y)
+DT = delaunayTriangulation(random_x, random_y);
+% indecies of vertices of the covexhull
+k = convexHull(DT);
+anchors = [(3*DT.Points(k(1:end-1),1)-1)'; (3*DT.Points(k(1:end-1),2)-1)'];
+size_x = size(anchors,1);
 num_anchors = size(anchors,2);
 % find number of sensors 
-num_sensors = 10;
+num_sensors = 40;
 % generate them
-[sensors, dx, da] = generate_sensor(anchors,num_sensors,0);
+[sensors, dx, da] = generate_sensor(anchors,num_sensors,1, DT, k);
 
 % Go from index to x,y coordinat
 id_xy = @(i,rows, cols) ([ (mod(i,rows) == 0)*rows + mod(i,rows)  , ceil(i/rows)]);
@@ -16,7 +23,8 @@ id_xy = @(i,rows, cols) ([ (mod(i,rows) == 0)*rows + mod(i,rows)  , ceil(i/rows)
 id = @(row,col,rows, columns) ((col-1)*rows + row);
 
 %% get the indices of the upper quadrant of the d_x matrix
-upper_quadrant = zeros(1, num_sensors^2/2 - num_sensors);
+
+upper_quadrant = zeros(1, round(num_sensors^2/2) - num_sensors);
 k = 1;
 for i=1:num_sensors
     for j=i+1:num_sensors
@@ -26,7 +34,7 @@ for i=1:num_sensors
 end
 
 % Number of sensors for which we have pairwise distance information
-num_Nx = round(numel(upper_quadrant)/4);
+num_Nx = round(numel(upper_quadrant));
 % Index of sensors for which we have anchor information
 Nx = upper_quadrant(randperm(numel(upper_quadrant), num_Nx))';
 % find x,y distance
@@ -41,8 +49,9 @@ end
 
 Pairwise_Sensor_Distance = horzcat(X_x, dx(Nx));
 %%
-
-num_Na = round(numel(da)/4);
+% -2*num_sensors works only for num_sensors>5, otherwise it needs to be
+% smaller.
+num_Na = round(numel(da))-2.5*num_sensors;
 Na = randperm(numel(da), num_Na)';
 X_a = id_xy(Na, num_sensors, num_anchors);
 
@@ -51,7 +60,15 @@ for i=1:size(X_a,1)
     tmpA(i) = da(X_a(i,1), X_a(i,2));
 end
 
-da(Na) - tmpA
 Sensor_Anchor_Distance = horzcat(X_a, da(Na));
 
-SDP(num_sensors, Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors)
+%% solves the SDP Relaxation Problem.
+Z = SDP(num_sensors, Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors);
+estimated_sensors = Z(length(anchors(:,1))+1:end, 1:size_x)';
+
+error_sensors = ((sensors(1, :) - estimated_sensors(1, :)).^2 + ...
+    (sensors(2, :) - estimated_sensors(2, :)).^2).^.5;
+
+
+%% 3d plot
+ThreeDVerticleBarPlot(estimated_sensors, sensors, error_sensors)
