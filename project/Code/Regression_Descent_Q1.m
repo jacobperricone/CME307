02a2545a570e800b126df81f85c947e8141f7a0e
Jacob Perricone <jacobperricone@users.noreply.github.com>
2017-03-09 18:50:44 -0800

    Delete Regression_Descent_Q1.m

--- project/Code/Regression_Descent_Q1.m
+++
@@ -1,6 +0,0 @@
-function [estimated_sensors] = Regression_Descent_Q1(f,df, x num_sensors, Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors)
-
-    %% find number of unique sensors involved in the optimization
-    
-
-end
