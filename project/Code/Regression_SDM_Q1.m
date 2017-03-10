e2a10e5075b634c9cffc0648ecee2a0b1527522f
Jacob Perricone <jacobperricone@users.noreply.github.com>
2017-03-09 19:44:01 -0800

    Update Regression_SDM_Q1.m

--- project/Code/Regression_SDM_Q1.m
+++ project/Code/Regression_SDM_Q1.m
@@ -1,6 +1,22 @@
 function [estimated_sensors] = Regression_SDM_Q1(f,df, x_initial, MAX_ITER, TOL, ALPHA, num_sensors,  Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors, debug)
-
-    %% find number of unique sensors involved in the optimization
+%
+% Description: Implements the Steepest Descent Method on an initial point
+% x_initial, generates plots of function value and gradient value over time
+% Inputs
+    % f: function to optimize over-- takes arguments in the form
+    %    f(Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors, x)
+    %
+    % df: gradient function that takes arguments in the form
+    %     df(num_sensors, Pairwise_Sensor_Distance, Sensor_Anchor_Distance, anchors, x)
+    %     where x is the point at which to calculate the gradient 
+    %
+    % num_sensors: number of sensors to estimate, 
+    % anchors: the known anchors
+    % Pairwise_Sensor_Distance: sensor1_id, sensor2_id, distance
+    % Sensor_Anchor_Distance: sensor_id, anchor_id, distance
+    % MAX_ITER, TOL, ALPHA, debug all kind of obvious
+% Returns: 
+%   estimated_sensors: estimated sensors 
     
     x_prev = x_initial;
     
@@ -11,6 +27,7 @@ function [estimated_sensors] = Regression_SDM_Q1(f,df, x_initial, MAX_ITER, TOL,
     psd = Pairwise_Sensor_Distance;
     sad = Sensor_Anchor_Distance;
     fvals(iter) = f(psd, sad, anchors, x_prev);
+    df(num_sensors,psd,sad,anchors,x_prev)
     gvals(iter) = norm(df(num_sensors,psd,sad,anchors,x_prev));
     
     
@@ -21,7 +38,7 @@ function [estimated_sensors] = Regression_SDM_Q1(f,df, x_initial, MAX_ITER, TOL,
             disp(sprintf('----GRADIENT NORM IS BELOW TOLERANCE CONVERGENCE OF FUNCTION AFTER %d ITERATIONS-----', iter))
             disp(sprintf('-----------------------FINAL Iteration: %d--------------------------------', iter));
             disp('  f(x)    delta_f(x)    g(x)     delta_g(x)   NORMGRAD')
-            disp([  fvals(iter), fvals(iter) - fvals(iter-1), gvals(iter), gvals(iter) - gvals(iter) - 1 ])
+            disp([  fvals(iter), fvals(iter) - fvals(iter-1), gvals(iter), gvals(iter) - gvals(iter)])
             break;
         end
 
@@ -35,7 +52,7 @@ function [estimated_sensors] = Regression_SDM_Q1(f,df, x_initial, MAX_ITER, TOL,
         
         if debug
             disp(sprintf('-----------------------Iteration: %d--------------------------------', iter));
-            disp('  f(x)    delta_f(x)    g(x)     delta_g(x)   NORMGRAD')
+            disp('  f(x)       delta_f(x)       g(x)        delta_g(x)      NORMGRAD')
             disp([  fvals(iter), fvals(iter) - fvals(iter-1), gvals(iter), gvals(iter) - gvals(iter) ])
         end
         
@@ -48,6 +65,21 @@ function [estimated_sensors] = Regression_SDM_Q1(f,df, x_initial, MAX_ITER, TOL,
     
     
     
-    
+s = 'Regression';
+fig = figure();
+subplot(2,1,1)
+plot(1:iter, gvals, 'LineWidth',2); grid on;
+title(strcat({' Norm Gradient Function SDM Method: '}, s));
+xlabel('Iteration'); ylabel('G(x)');
+
+subplot(2, 1, 2)
+plot(1:iter, fvals, 'LineWidth',2); grid on;
+title(strcat({'Objective Function SDM Method: '}, s));
+
+xlabel('Iteration'); ylabel('F(x)');
+
+save_string = [pwd strcat('/FIGURES/Question1/SDM', '_', s,'.png')];
+saveas(fig, save_string)
+
 
 end
